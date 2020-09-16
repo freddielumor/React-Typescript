fdcfadc40ec8de338d954273d7eb577f1960e506
Freddie Lumor <freddielumor@gmail.com>
2020-09-16 23:17:31 +0100

    Fix filename typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Players from './pages/Players';
 import Teams from './pages/Teams';
-import { TEAMS_DATA_ENDPOINT } from './utils/apiCongig';
+import { TEAMS_DATA_ENDPOINT } from './utils/apiConfig';
 import './app.scss';
 
 export interface teamDataItem {
